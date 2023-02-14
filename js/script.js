8495cd53c6b459a27114cad91902d54121814745
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-02-14 17:13:21 +0500

    added staff page for all movies(actors, directors etc.), made header nav w/ links for dif types of movies collections, made nice search input

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ const API_KEY = '8c8e1a50-6322-4135-8875-5d40a5420d86'
 const API_KEY_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1'
 const API_SEARCH_URL = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
 const API_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
+const API_ACTORS = `https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=`
+const API_TOP250_URL = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1'
+const API_AWAIT_FILMS = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS&page=1'
+
 
 const form = document.querySelector('form')
 const search = document.querySelector('.header__search')
+const searchIcon = document.querySelector('.fa-solid.fa-magnifying-glass')
 
 getMovies(API_KEY_POPULAR)
 
@@ -56,8 +61,8 @@ function showMovies(data){
         <div class="movie__cover--darker"></div>
       </div>
       <div class="movie__info">
-        <div class="movie__title">${movie.nameRu}</div>
-        ${movie.rating.includes('%') ?  '' : ` <div class="movie__average movie__average--${movieRatingCount(movie.rating)}">${movie.rating}</div>`}
+        ${movie.nameRu ? `<div class="movie__title">${movie.nameRu}</div>`  : '<div class="movie__title">Нет названия</div>' }
+        ${!movie.rating  ?  '' : ` <div class="movie__average movie__average--${movieRatingCount(movie.rating)}">${movie.rating}</div>`} 
       </div>
         `
         movieEl.addEventListener('click', () => openModal(movie.filmId))
@@ -77,7 +82,7 @@ form.addEventListener('submit', (e) => {
   
 })
 
-
+// Modal window
 
 const modalEl = document.querySelector('.modal')
 
@@ -100,19 +105,23 @@ modalEl.innerHTML = `
   <div class="modal__button-close">X</div>
     <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
     <h2>
-      <span class="modal__movie-title"> ${respData.nameRu}</span>
+      ${respData.nameRu ? `<span class="modal__movie-title"> ${respData.nameRu}</span>` : '<span class="modal__movie-title">Нет названия</span>'}
       <span class="modal__movie-release-year">(${respData.year})</span>
     </h2>
     <ul class="modal__movie-info">
       <li class="modal__movie-category">Жанр: ${respData.genres.map(genre => ` ${genre.genre}`)}</li>
-      ${respData.filmLength ? `<li class="modal__movie-runtime">Продолжительность - ${respData.filmLength} минут</li>` : ''}
-      <li>Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
-      <li class="modal__movie-overview"> Описание: ${respData.description}</li>
+      ${respData.filmLength ? `<li class="modal__movie-runtime">Продолжительность: ${respData.filmLength} минут</li>` : ''}
+      <li>Ссылка на КП: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+      <li><a class="modal__staff">Каст</a></li>
+      ${respData.description ? `<li class="modal__movie-overview"> Описание: ${respData.description}</li>` : '<li class="modal__movie-overview">Нет описания</li>'}
     </ul>
   </div>
 `
 const btnClose = document.querySelector('.modal__button-close')
+const cast = document.querySelector('.modal__staff')
 btnClose.addEventListener('click', () => closeModal())
+cast.addEventListener('click', () => closeModal())
+cast.addEventListener('click', () => getCast(respData.kinopoiskId))
 }
 
 function closeModal(){
@@ -131,3 +140,62 @@ window.addEventListener('keydown', (e) => {
     closeModal()
   }
 })
+
+// Modal window ends
+
+searchIcon.addEventListener('click', ()=>{
+  search.classList.toggle('hidden')
+})
+
+
+async function getCast(id){
+  const actorsResp = await fetch(API_ACTORS + id, {
+    headers:{
+        "Content-Type": "application/json",
+        "X-API-KEY": API_KEY
+    },
+})
+
+const respDataActors = await actorsResp.json()
+
+showCast(respDataActors)
+}
+
+
+
+function showCast(data){
+  const moviesEl = document.querySelector('.movies')
+  moviesEl.innerHTML = ''
+
+  data.forEach(person => {
+
+    const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+        <div class="movie__cover-inner">
+        <img
+          src="${person.posterUrl}"
+          class="movie__cover"
+          alt = "${person.nameRu}"
+        />
+        <div class="movie__cover--darker"></div>
+      </div>
+      <div class="movie__info">
+        <div class="movie__title">${person.nameRu}</div>
+        ${!person.description ? `<div class="person__profession">${person.professionKey}` : `<div class="person__profession">${person.description}</div>`}
+      </div>
+        `
+       
+        moviesEl.appendChild(movieEl)
+  })
+}
+
+
+
+const top250Movies = document.querySelector('.header__top250')
+const awaitMovies = document.querySelector('.header__await-movies')
+const popularMovies = document.querySelector('.header__popular-movies')
+
+top250Movies.addEventListener('click', () => getMovies(API_TOP250_URL))
+awaitMovies.addEventListener('click', () => getMovies(API_AWAIT_FILMS))
+popularMovies.addEventListener('click', () => getMovies(API_KEY_POPULAR))
