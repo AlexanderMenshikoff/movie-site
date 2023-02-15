0e177f6816bea12fd40ae8e11036e5e4b37ab004
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-02-16 03:08:14 +0500

    made modal window for stuff, made modal window style & inside things, made stuff filmography page made vicious circle w/ stuff and movies modal windows

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const API_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
 const API_ACTORS = `https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=`
 const API_TOP250_URL = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1'
 const API_AWAIT_FILMS = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS&page=1'
-
+const API_ACTORS_MORE_INFO = 'https://kinopoiskapiunofficial.tech/api/v1/staff/'
 
 const form = document.querySelector('form')
 const search = document.querySelector('.header__search')
@@ -62,13 +62,14 @@ function showMovies(data){
       </div>
       <div class="movie__info">
         ${movie.nameRu ? `<div class="movie__title">${movie.nameRu}</div>`  : '<div class="movie__title">Нет названия</div>' }
-        ${!movie.rating  ?  '' : ` <div class="movie__average movie__average--${movieRatingCount(movie.rating)}">${movie.rating}</div>`} 
+        ${(movie.rating === 'null') || (!movie.rating)   ?  '' : ` <div class="movie__average movie__average--${movieRatingCount(movie.rating)}">${movie.rating}</div>`} 
       </div>
         `
         movieEl.addEventListener('click', () => openModal(movie.filmId))
         moviesEl.appendChild(movieEl)
     });
-}
+  }
+
 
 
 form.addEventListener('submit', (e) => {
@@ -112,7 +113,7 @@ modalEl.innerHTML = `
       <li class="modal__movie-category">Жанр: ${respData.genres.map(genre => ` ${genre.genre}`)}</li>
       ${respData.filmLength ? `<li class="modal__movie-runtime">Продолжительность: ${respData.filmLength} минут</li>` : ''}
       <li>Ссылка на КП: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
-      <li><a class="modal__staff">Каст</a></li>
+      <li><a class="modal__staff">Посмотреть каст → </a></li>
       ${respData.description ? `<li class="modal__movie-overview"> Описание: ${respData.description}</li>` : '<li class="modal__movie-overview">Нет описания</li>'}
     </ul>
   </div>
@@ -178,18 +179,66 @@ function showCast(data){
           class="movie__cover"
           alt = "${person.nameRu}"
         />
-        <div class="movie__cover--darker"></div>
+        <div class="person__cover--darker"></div>
       </div>
       <div class="movie__info">
         <div class="movie__title">${person.nameRu}</div>
         ${!person.description ? `<div class="person__profession">${person.professionKey}` : `<div class="person__profession">${person.description}</div>`}
       </div>
         `
-       
+        movieEl.addEventListener('click', () => openModalStaff(person.staffId))
         moviesEl.appendChild(movieEl)
   })
 }
 
+
+
+async function openModalStaff(id) {
+  const resp = await fetch(API_ACTORS_MORE_INFO + id, {
+    headers:{
+        "Content-Type": "application/json",
+        "X-API-KEY": API_KEY
+    },
+
+})
+
+
+const respData = await resp.json()
+
+  modalEl.classList.add('modal--show')
+  document.body.classList.add('stop-scrolling')
+
+modalEl.innerHTML = `
+  <div class="modal__card">
+  <div class="modal__button-close">X</div>
+    <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+    <h2>
+      ${respData.nameRu ? `<span class="modal__movie-title"> ${respData.nameRu}</span>` : '<span class="modal__movie-title">Нет информации</span>'}
+      
+    </h2>
+    <ul class="modal__movie-info">
+    <li class="modal__movie-category">Деятельность: ${respData.profession}</li>
+    ${respData.sex === 'MALE' ? '<li class="modal__movie-category">Пол: Мужской</li>' : '<li class="modal__movie-category">Пол: Женский</li>'}
+      
+      
+      ${respData.birthday ? `<li class="modal__movie-category">Дата рождения: ${respData.birthday}</li>` : ''}
+      ${respData.birthplace ? `<li class="modal__movie-category">Место рождения: ${respData.birthplace}</li>` : ''} 
+      ${respData.death ? `<li class="modal__movie-category">Дата смерти: ${respData.death}</li>` : ''} 
+      ${respData.deathplace ? `<li class="modal__movie-category">Место смерти: ${respData.deathplace}</li>` : ''}
+      ${respData.age > 0 ? `<li class="modal__movie-category">Возраст: ${respData.age}</li>` : ''}
+      ${respData.growth ? `<li class="modal__movie-category">Рост: ${respData.growth}</li>` : ''}
+      <li>Ссылка на КП: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+      <li><a class="modal__staff">Фильмография → </a></li>
+    </ul>
+  </div>
+`
+
+const btnClose = document.querySelector('.modal__button-close')
+const staffMovies = document.querySelector('.modal__staff')
+btnClose.addEventListener('click', () => closeModal())
+staffMovies.addEventListener('click', () => getStaffMovies(API_ACTORS_MORE_INFO + id))
+staffMovies.addEventListener('click', () => closeModal())
+}
 
 
 const top250Movies = document.querySelector('.header__top250')
@@ -199,3 +248,43 @@ const popularMovies = document.querySelector('.header__popular-movies')
 top250Movies.addEventListener('click', () => getMovies(API_TOP250_URL))
 awaitMovies.addEventListener('click', () => getMovies(API_AWAIT_FILMS))
 popularMovies.addEventListener('click', () => getMovies(API_KEY_POPULAR))
+
+
+
+async function getStaffMovies(url){
+  const resp = await fetch(url, {
+      headers:{
+          "Content-Type": "application/json",
+          "X-API-KEY": API_KEY
+      },
+
+  })
+  const respData = await resp.json()
+  showStaffMovies(respData)
+}
+
+function showStaffMovies(data){
+  const moviesEl = document.querySelector('.movies')
+
+  // очищаем предыдущие фильмы
+  moviesEl.innerHTML = ''
+
+  data.films.forEach(movie => {
+      const movieEl = document.createElement('div')
+      movieEl.classList.add('movie')
+      movieEl.innerHTML = `
+      <div class="movie__cover-inner">
+      ${movie.posterUrl  ? `<img src="${movie.posterUrl}" class="movie__cover" alt = "${movie.nameRu}" />` : '<div class = "black-cover__stuff"></div>'}
+      ${!movie.posterUrl ? `<div class = "staff__profession">${movie.professionKey}</div>` : '' } 
+      ${!movie.posterUrl ? `<div class = "staff__description">${movie.description}</div>` : ''}
+      ${!movie.posterUrl ? `<div class = "poster__info">Нажмите, чтобы увидеть постер</div>` : ''}
+    </div>
+    <div class="movie__info">
+      ${movie.nameRu ? `<div class="movie__title">${movie.nameRu}</div>`  : '<div class="movie__title">Нет названия</div>' }
+      ${(movie.rating === 'null') || (!movie.rating)   ?  '' : ` <div class="movie__average movie__average--${movieRatingCount(movie.rating)}">${movie.rating}</div>`} 
+    </div>
+      `
+      movieEl.addEventListener('click', () => openModal(movie.filmId))
+      moviesEl.appendChild(movieEl)
+  });
+}
